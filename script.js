5408c2e48d602bdf1d35ae0557e303ec788edc6e
SabitTheBot <iamsabit99@gmail.com>
2020-09-03 17:59:33 +0600

    add new page functionality

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-
+let a;
 function findImg(name){
     console.log(name);
+    localStorage.setItem("someVarKey", name); ;
+     
+}
+
+
+function show(){
+    //console.log(a);
+    document.getElementById("heading").innerHTML =localStorage.getItem("someVarKey");
+ //   $("#imageShow").attr("src", );
+    document.getElementById("imageShow").src=  "images/"+localStorage.getItem("someVarKey")+'.png'
 }
